6240aaa78e714144b7fa1b86f81c8661694019f8
dylan-yoon <d.dylany21@gmail.com>
2024-02-02 00:26:41 +0900

    feat: 필요한 MainDTO 생성 및 Network 임시구현

---
+++ PrographProject/PrographProject/Source/MainImageDataDTO.swift
@@ -0,0 +1,51 @@
+//
+//  ImageDataDTO.swift
+//  PrographProject
+//
+//  Created by Dylan_Y on 2/1/24.
+//
+
+import Foundation
+
+struct MainImageDataDTO: Decodable {
+    let id: String
+    let width: Int
+    let height: Int
+    let description: String?
+    let urls: Urls
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case width, height
+        case description
+        case urls
+    }
+}
+
+// MARK: - Urls
+struct Urls: Codable {
+    let raw: String
+    let full: String
+    let regular: String
+    let small: String
+    let thumb: String
+    let smallS3: String
+
+    enum CodingKeys: String, CodingKey {
+        case raw, full, regular, small, thumb
+        case smallS3 = "small_s3"
+    }
+}
+// MARK: - Links
+struct Links: Codable {
+    let linksSelf: String
+    let html: String
+    let download: String
+    let downloadLocation: String
+
+    enum CodingKeys: String, CodingKey {
+        case linksSelf = "self"
+        case html, download
+        case downloadLocation = "download_location"
+    }
+}
---
+++ PrographProject/PrographProject/Source/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  PrographProject
+//
+//  Created by Dylan_Y on 2/1/24.
+//
+
+import Foundation
